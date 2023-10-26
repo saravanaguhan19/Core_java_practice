c040579ce495ca1f87110ba6f7722f456f5bc944
saravanaguhan19 <saravanaguhan19@gmail.com>
2023-10-26 11:27:33 +0530

    jumping statement practice

---
+++ Break.java
@@ -0,0 +1,22 @@
+public class Break {
+
+    public static void main(String[] args) {
+        boolean t = true;
+
+        first: {
+            second: {
+                third:{
+                    System.out.println("Before the break.");
+                    if(t){
+                        break second;
+                    }
+                    System.out.println("This won't execute 1");
+                
+                }
+                System.out.println("This won't execute 2");
+            }
+            System.out.println("This is after second block");
+        }
+    }
+    
+}
